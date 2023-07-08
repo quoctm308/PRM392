db1df96757f9c210a9c6f4c67f53f1a9be6d3224
mtq308 <tranminhquoc945@gmail.com>
2023-07-08 18:03:51 +0700

    Boardcast receiver complete

---
+++ Slot_16/boardcast_reveiver/app/src/main/java/com/example/boardcast_reveiver/MainActivity1.java
@@ -0,0 +1,31 @@
+package com.example.boardcast_reveiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+
+public class MainActivity1 extends AppCompatActivity {
+    DynamicConnectedReceiver dynamicConnectedReceiver = new
+            DynamicConnectedReceiver();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter filter = new
+                IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(dynamicConnectedReceiver, filter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+}
